aa7a6cdd8631b6a2010665dd42242411f0337358
aalcolea <alanalcolea@gmail.com>
2025-03-01 12:51:33 -0600

    reseting scritps

--- resources/views/Tienda/Embriones/embrionesHome.blade.php
+++ resources/views/Tienda/Embriones/embrionesHome.blade.php
@@ -3,7 +3,9 @@
 <head>
 	<title>Embriones - Ganado Yucatán</title>
 </head>
-
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<script src="{{url('/static/js/location.js') }}" ></script>
 	<!-- //TODO Nueva sección de tienda -->
 	<div class="tienda-section">
 		<div class="banner-main-embriones">
