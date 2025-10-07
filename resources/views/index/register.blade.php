f01c3a8be718f7d6c23b14d00ddc5a58d60dda0b
aalcolea <alanalcolea@gmail.com>
2025-10-07 01:15:42 -0600

    addings changes to register and fierros pics

--- resources/views/index/register.blade.php
+++ resources/views/index/register.blade.php
@@ -149,7 +149,7 @@
                     <label for="politicasPrivacidad">Acepto las <a href="/politicaPrivacidad" class="privacy-policy-link">políticas de privacidad</a></label>
                 </div>
                 @php
-                    $dir = public_path('userspics');
+                    $dir = public_path('public/userspics');
                     $imagenes = [];
                     if (is_dir($dir)) {
                         $files = glob($dir . '/*_fierro.webp');
